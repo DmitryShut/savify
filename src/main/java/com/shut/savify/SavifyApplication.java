1a2cd4be9f266e50c924325760da942902a64e4e
shutd <shut.dmitry35@gmail.com>
2019-08-06 20:29:25 +0300

    init

---
+++ src/main/java/com/shut/savify/SavifyApplication.java
@@ -0,0 +1,13 @@
+package com.shut.savify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SavifyApplication.class, args);
+    }
+
+}
