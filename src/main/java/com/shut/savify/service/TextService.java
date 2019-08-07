6edd589103904eb359e9e6772cba63190a5947ad
shutd <shut.dmitry35@gmail.com>
2019-08-07 19:48:50 +0300

    feat: add key hashing

--- src/main/java/com/shut/savify/service/TextService.java
+++ src/main/java/com/shut/savify/service/TextService.java
@@ -2,6 +2,7 @@ package com.shut.savify.service;
 
 import com.shut.savify.entity.Text;
 import com.shut.savify.repository.TextRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -9,9 +10,11 @@ import java.util.Optional;
 @Service
 public class TextService {
     private final TextRepository textRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public TextService(TextRepository textRepository) {
+    public TextService(TextRepository textRepository, PasswordEncoder passwordEncoder) {
         this.textRepository = textRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public Optional<Text> getText(String key){
@@ -19,6 +22,7 @@ public class TextService {
     }
 
     public Text createText(Text text){
+        text.setKey(passwordEncoder.encode(text.getKey()));
         return textRepository.save(text);
     }
 
