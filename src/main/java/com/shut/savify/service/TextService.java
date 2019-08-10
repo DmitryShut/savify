9809e0ecdfdf7eba551ee3b283fb8fed55c4f19e
shutd <shut.dmitry35@gmail.com>
2019-08-11 01:42:31 +0300

    feat: add encoding and decoding

--- src/main/java/com/shut/savify/service/TextService.java
+++ src/main/java/com/shut/savify/service/TextService.java
@@ -1,29 +1,47 @@
 package com.shut.savify.service;
 
 import com.shut.savify.entity.Text;
+import com.shut.savify.exception.NotFoundException;
 import com.shut.savify.repository.TextRepository;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import com.shut.savify.util.CryptographyService;
+import lombok.SneakyThrows;
 import org.springframework.stereotype.Service;
 
+import java.security.KeyPair;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 import java.util.Optional;
 
 @Service
 public class TextService {
     private final TextRepository textRepository;
-    private final PasswordEncoder passwordEncoder;
 
-    public TextService(TextRepository textRepository, PasswordEncoder passwordEncoder) {
+    public TextService(TextRepository textRepository) {
         this.textRepository = textRepository;
-        this.passwordEncoder = passwordEncoder;
     }
 
-    public Optional<Text> getText(String key){
-        return textRepository.findById(key);
+    @SneakyThrows
+    public String getText(String key) {
+        PrivateKey pvt = CryptographyService.getPrivateKeyFromString(key.replace(' ','+'));
+        PublicKey publicKey = CryptographyService.getPublicKey(pvt);
+        String base64PublicKey = CryptographyService.hashPublicKey(publicKey);
+        Optional<Text> base64EncodedText = textRepository.findById(base64PublicKey);
+        if(base64EncodedText.isPresent()){
+            return CryptographyService.decodeText(base64EncodedText.get().getText(), pvt);
+        }else {
+            throw new NotFoundException("Text with this key not found");
+        }
     }
 
-    public Text createText(Text text){
-        text.setKey(passwordEncoder.encode(text.getKey()));
-        return textRepository.save(text);
+    @SneakyThrows
+    public String createText(Text text) {
+        KeyPair keyPair = CryptographyService.getKeyPair();
+        PrivateKey privateKey = keyPair.getPrivate();
+        PublicKey publicKey = CryptographyService.getPublicKey(privateKey);
+        String base64EncryptedTextString = CryptographyService.getEncryptedText(text.getText(), publicKey);
+        String base64PublicKey = CryptographyService.hashPublicKey(publicKey);
+        textRepository.save(new Text(base64PublicKey, base64EncryptedTextString));
+        return CryptographyService.toStringPrivateKey(privateKey);
     }
 
 }
